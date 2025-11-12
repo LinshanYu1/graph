8e594e0a28512386e1c212236d7d4263a87358ab
LinshanYu1 <lyu4@wpi.edu>
2025-11-12 01:39:51 -0500

    init

--- ucs.py
+++ ucs.py
@@ -6,23 +6,26 @@ def ucs(graph, start, end):
     frontier = []
     heapq.heappush(frontier, (0, start, [start])) # total cost, current node, current path
     visited = set()
+    node_cost = {node: float('inf') for node in graph}
+    node_cost[start] = 0
 
     while frontier:
         cost, node, path = heapq.heappop(frontier)
-
-        if node in visited:
+        if cost > node_cost[node]:
             continue
-        visited.add(node)
 
-        if node == end :
+        if node == end:
             return cost, path
+
 
         for neighbor, weight in graph[node].items():
             if neighbor in visited:
                 continue
 
             total_cost = cost + weight
-            heapq.heappush(frontier, (total_cost, neighbor, path + [neighbor]))
-
+            if total_cost < node_cost[neighbor]:
+                node_cost[neighbor] = total_cost
+                heapq.heappush(frontier, (total_cost, neighbor, path + [neighbor]))
+        visited.add(node)
 
     return float('inf'), []
